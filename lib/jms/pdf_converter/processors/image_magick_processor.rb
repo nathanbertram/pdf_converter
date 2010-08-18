05c862492e3e7ba71088bae151f923a3bec2a7bc
Nathan Bertram <nbertram@gmail.com>
2010-08-18 17:24:20 -0500

    Added: Quality of jpg/png returned note

--- lib/jms/pdf_converter/processors/image_magick_processor.rb
+++ lib/jms/pdf_converter/processors/image_magick_processor.rb
@@ -13,6 +13,8 @@ module Jms
             raise Jms::PdfConverter::PDFError, "File is not a pdf" 
           end
           
+          # If you are having trouble with the legibility of the png's or jpg's returned try these options:
+          # convert -verbose -colorspace RGB -resize 800 -interlace none -density 300 -quality 80 input.pdf output.jpg
           cmd = "convert #{pdf_options[:processor_options].shift.to_s} #{path} #{pdf_path}"
           `#{cmd}`
           if $? != 0
